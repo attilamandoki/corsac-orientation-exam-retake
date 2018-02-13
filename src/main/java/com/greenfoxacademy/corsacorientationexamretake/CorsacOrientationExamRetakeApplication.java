3511d90dc8b1c178dc5dae60dafaf3e4ee1ee1d6
attilamandoki <attilamandoki@gmail.com>
2018-02-13 10:03:21 +0100

    feat(app)first commit: Created model classes, after a long fight with MySQL managed to set up DB connection, saved screenshots

---
+++ src/main/java/com/greenfoxacademy/corsacorientationexamretake/CorsacOrientationExamRetakeApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.corsacorientationexamretake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorsacOrientationExamRetakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorsacOrientationExamRetakeApplication.class, args);
+	}
+}
---
+++ src/main/java/com/greenfoxacademy/corsacorientationexamretake/models/Ticket.java
@@ -0,0 +1,77 @@
+package com.greenfoxacademy.corsacorientationexamretake.models;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Ticket {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
+  private String reporter;
+  private String manufacturer;
+  private long serialnumber;
+  private String description;
+  @Column(name = "reported_at")
+  private Date reportedAt;
+
+  public Ticket() {
+  }
+
+  public Ticket(String reporter, String manufacturer, long serialnumber, String description, Date reportedAt) {
+    this.reporter = reporter;
+    this.manufacturer = manufacturer;
+    this.serialnumber = serialnumber;
+    this.description = description;
+    this.reportedAt = reportedAt;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getReporter() {
+    return reporter;
+  }
+
+  public void setReporter(String reporter) {
+    this.reporter = reporter;
+  }
+
+  public String getManufacturer() {
+    return manufacturer;
+  }
+
+  public void setManufacturer(String manufacturer) {
+    this.manufacturer = manufacturer;
+  }
+
+  public long getSerialnumber() {
+    return serialnumber;
+  }
+
+  public void setSerialnumber(long serialnumber) {
+    this.serialnumber = serialnumber;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public Date getReportedAt() {
+    return reportedAt;
+  }
+
+  public void setReportedAt(Date reportedAt) {
+    this.reportedAt = reportedAt;
+  }
+}
